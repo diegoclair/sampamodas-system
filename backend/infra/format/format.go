aeed4c7b949dbc8f48c820267658c1cd64a5d3dc
Diego Clair Rodrigues <diego93rodrigues@gmail.com>
2021-01-20 23:42:31 -0300

    Added toUpperCase  function

--- backend/infra/format/format.go
+++ backend/infra/format/format.go
@@ -2,6 +2,12 @@ package format
 
 import "strings"
 
-func FirstLetterUppercase(str *string) {
+//FirstLetterUpperCase - returns upper case of all first letter of the words
+func FirstLetterUpperCase(str *string) {
 	*str = strings.Title(strings.ToLower(*str))
+}
+
+//ToUpperCase - transform all string to upper case
+func ToUpperCase(str *string) {
+	*str = strings.ToUpper(*str)
 }
