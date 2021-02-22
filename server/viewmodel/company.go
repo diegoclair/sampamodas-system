f53e3488f988bd59046ae5094df44a418a8ab9cb
Diego Clair Rodrigues <diego93rodrigues@gmail.com>
2021-02-22 01:20:53 -0300

    fix viewmodels

--- server/viewmodel/company.go
+++ server/viewmodel/company.go
@@ -2,7 +2,7 @@ package viewmodel
 
 // Company viewmodel
 type Company struct {
-	CompanyID      int64  `json:"company_id,omitempty"`
+	CompanyID      int64  `json:"company_id,omitempty" mapper:"ID"`
 	DocumentNumber string `json:"document_number,omitempty"`
 	CommercialName string `json:"commercial_name,omitempty"`
 	LegalName      string `json:"legal_name,omitempty"`
--- server/viewmodel/product.go
+++ server/viewmodel/product.go
@@ -15,9 +15,9 @@ type Product struct {
 // ProductStock viewmodel
 type ProductStock struct {
 	ID                int64  `json:"id,omitempty"`
-	Color             string `json:"color,omitempty"`
+	Color             string `json:"color,omitempty" mapper:"Color.Name"`
 	Size              string `json:"size,omitempty"`
-	AvailableQuantity string `json:"available_quantity,omitempty"`
+	AvailableQuantity int64  `json:"available_quantity,omitempty"`
 	Quantity          int64  `json:"quantity,omitempty" mapper:"InputQuantity"`
 }
 
