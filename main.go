7cb05f9ad480f66f9d81092ffcaf2be837232bb6
Diego Clair <diego93rodrigues@gmail.com>
2021-08-21 14:26:01 -0300

    feat: add copyright name

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+Copyright © 2021 Diego Clair Rodrigues <diego93rodrigues@gmail.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
